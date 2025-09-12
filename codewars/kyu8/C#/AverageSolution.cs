b942438dac267c6689c943c14e4926429f662120
Lendon <ranierlendon11@gmail.com>
2025-09-12 23:39:53 +0800

    solution for average solution

---
+++ codewars/kyu8/C#/AverageSolution.cs
@@ -0,0 +1,11 @@
+using System.Linq;
+
+// Source: https://www.codewars.com/kata/57a2013acf1fa5bfc4000921/train/csharp
+
+class AverageSolution
+{
+    public static double FindAverage(double[] array)
+    {
+        return array.Average();
+    }
+}
