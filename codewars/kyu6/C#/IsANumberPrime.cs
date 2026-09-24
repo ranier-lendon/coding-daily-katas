66c2c1553c4411afd6c6b6d6d953d24b03a9900c
Lendon <ranierlendon11@gmail.com>
2026-09-24 16:14:38 +0800

    IsANumberPrime Solution

---
+++ codewars/kyu6/C#/IsANumberPrime.cs
@@ -0,0 +1,21 @@
+using System;
+
+public static class Kata
+{
+  public static bool IsPrime(int n)
+  {
+    if (n <= 1) return false;
+        
+    if (n == 2) return true;
+
+    if (n % 2 == 0) return false;
+
+    int boundary = (int)Math.Floor(Math.Sqrt(n));
+
+    for (int i = 3; i <= boundary; i += 2)
+    {
+        if (n % i == 0)  return false;
+    }
+    return true;
+  }
+}
