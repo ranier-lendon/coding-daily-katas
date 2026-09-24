e7a790511e107a240df53b55dce63035b752d318
Lendon <ranierlendon11@gmail.com>
2026-09-24 15:38:24 +0800

    AddTwoNumbers Solution

---
+++ leetcode/AddTwoNumbers.cs
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
+        var dummyHead = new ListNode();
+        var curr = dummyHead;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0)
+        {
+            int val1 = (l1 != null) ? l1.val : 0;
+            int val2 = (l2 != null) ? l2.val : 0;
+            
+            int sum = val1 + val2 + carry;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            
+            curr = curr.next;
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+
+        return dummyHead.next;
+    }
+}
